a1ff59f34472dc7a9ee32215a921f24f50369cef
Sami Munir <smunir2001@gmail.com>
2022-12-11 16:08:10 -0500

    Chars.c

---
+++ Section 2 - Variables & Data Types/Chars.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int agrc, char* argv[]) {
+    char my_character = 's';
+    printf("my_character = %c", my_character);
+    return EXIT_SUCCESS;
+}
